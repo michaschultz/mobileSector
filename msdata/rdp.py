9802c3fcb1db2dceec496e2837e0348ba3613552
michaschultz <57436751+michaschultz@users.noreply.github.com>
2022-08-06 17:35:43 +0200

    update

--- msdata/rdp.py
+++ msdata/rdp.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 
-@staticmethod
+# @staticmethod
 def simplify(points: np.array, tolerance: float, anchors : list) -> list:
 	sqTolerance = tolerance * tolerance
 	# !!! ATTENTION python list starts with [] not as JAVA list with () !!!
