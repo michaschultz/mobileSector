6f4c631c363d485b96b3e07c29d6c0d5e6623f03
Seyed <seyed.fattahi@unibw.de>
2022-11-11 13:06:08 +0100

    Update please.py

--- skdata/please.py
+++ skdata/please.py
@@ -10,45 +10,7 @@ from random import randint
 from scipy.spatial import Voronoi, voronoi_plot_2d
 import matplotlib.pyplot as plt
  
-'''Clat2= 17.405098
-Clon2= 97.871728
-Clat3= 13.213485
-Clon3= 107.705434
-Clat4= 0.88282
-Clon4= 97.749317
-Clat5= -5.424946
-Clon5=  105.420423
-Clat6=  -8.517786
-Clon6=  117.57994
-Clat7=  0.10961
-Clon7=  113.336353
-Clat8=  0.80143
-Clon8=  124.516582
-Clat9= 11.300808
-Clon9= 123.570896
-Clat10=  -2.698363
-Clon10=  133.370998
-Clat11= 7.434758
-Clon11= 134.635915
 
-Clat2= 1.4976
-Clon2= 103.604
-Clat3= 1.49803
-Clon3= 103.8088
-Clat4= 1.3279
-Clon4= 103.60109
-Clat5= 1.322818
-Clon5=  103.810854
-Clat6=  1.4905
-Clon6=  104.041235
-Clat7=  1.30934
-Clon7=  104.04044
-Clat8= 1.18725
-Clon8=  103.6014
-Clat9= 1.18486
-Clon9= 103.8148
-Clat10= 1.187245
-Clon10= 104.0333'''
 ### Initialization function of your plugin. Do not change the name of this
 ### function, as it is the way BlueSky recognises this file as a plugin.
 def init_plugin():
