7821e2fed423f0565502738dc851bf4eaeaf8c9a
Seyed <seyed.fattahi@unibw.de>
2022-11-11 12:58:29 +0100

    Update dynpoly_sim.py

--- skdata/dynpoly_sim.py
+++ skdata/dynpoly_sim.py
@@ -40,7 +40,7 @@ def myfun():
     Clon4 = Clon - 1
   
     # Code to send updated polys
-    values =  [Clat1,Clon1,Clat2,Clon2,Clat3,Clon3,Clat4,Clon4]
+    # a poly with the following 4 nodes: [Clat1,Clon1,Clat2,Clon2,Clat3,Clon3,Clat4,Clon4]
    
     
     data = [
